de29110c5ee8bcabe5afcaa4751f3b4495c967c8
ganiefah <ganiefahmajiet3@gmail.com>
2023-05-26 13:48:09 +0200

    testimonial

--- script/code.js
+++ script/code.js
@@ -45,7 +45,7 @@ education.forEach((data)=> {
  
 //skills section
 //html
-var i = 0;
+ i = 0;
 function move() {
   if (i == 0) {
     i = 1;
@@ -124,3 +124,53 @@ function move3() {
     }
   }
 }
+
+//testimonials
+let testimonials= [{
+    id:1,
+    name: 'Rayhaan Khan',
+    image:  'https://i.postimg.cc/Y0bdBxXT/C12-Rayhaan-Khan-2.jpg',
+    description: "Ganiefah Majiet is the kindest-hearted and most dedicated woman I've ever met. She consistently manages to uplift the mood in a dark room and always strives to help everyone to the best of her ability. Once her mind is set on something, she ensures that she gives her utmost effort to excel. I highly recommend Ganiefah to everyone, as she would be a valuable addition to the workplace and is always seeking to improve.",
+},
+{
+    id: 2,
+    name: "Laiquah Cotie",
+    image: 'https://i.postimg.cc/cCRNRPLw/C12-Laiquah-Cotie-2.jpg',
+    description: "Ganiefah is a hard worker who never gives up and consistently applies herself, often going above and beyond what is required. It is a pleasure to have her on your team, as she is a reliable team player and performs well under pressure. She consistently delivers results when they are needed. Additionally, she has a helpful and cheerful personality.",
+
+   
+},
+{
+    id: 3,
+    name: "Eighton Lee Paulse",
+    image: 'https://i.postimg.cc/43fRzWDh/C12-C2-Eighton-Lee-Paulse-2.jpg',
+    description: "Ganiefah radiates vibrant and joyful energy. She is highly motivated in her work and puts her heart and soul into everything she does. Her personality is truly unique and enjoyable, and she shows great respect for everybody around her. Ganiefah brings only positive energy, always brightening up the mood wherever she goes. I wholeheartedly recommend her without hesitation.",
+
+},
+{
+    id: 4,
+    name: "Codi Kader",
+    image: 'https://i.postimg.cc/qBY96pX9/Codi-Kader.jpg',
+    description: "Ganiefah excels both in group settings and when working independently. She possesses the remarkable ability to adapt to any environment she finds herself in. Once you get to know her, you experience a genuine sense of belonging, as she welcomes you with warmth and kindness.Ganiefah's personality is wonderfully bubbly, making her a valuable addition to any workplace or business. She demonstrates exceptional proficiency in HTML & CSS. When collaborating with a team, she is the go-to person for assistance, always willing to lend a hand. This willingness to help is an invaluable asset when working in a team.",
+},
+{
+    id: 5,
+    name: "Uzukhanye Dywili",
+    image: 'https://i.postimg.cc/6Qn4vpNQ/C11-Uzukhanye-Dywili-4.jpg',
+    description: "Working with Ganiefah Majiet was an incredible experience. Their professionalism, attention to detail, and communication skills made the entire process seamless. They consistently exceeded my expectations and delivered exceptional results. I would highly recommend Ganiefah Majiet to anyone looking for a talented and dedicated professional.",
+
+},
+]
+let divTestimonials = document.querySelector('.testimonials');
+testimonials.forEach((data)=> {
+    divTestimonials.innerHTML += `
+    <div class="card">
+        <h4 class="display-4">${data.name}</h4>
+        <div id="card-body">
+               <img src= "${data.image}" class = "img-fluid">
+               <p class="text-black">${data.description}
+            </p>
+        </div>
+    </div>
+    `
+})
