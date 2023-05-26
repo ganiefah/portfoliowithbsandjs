217e61da792ba66992945b2b0e51e9abe475423d
ganiefah <ganiefahmajiet3@gmail.com>
2023-05-26 11:47:58 +0200

    lippe

--- script/code.js
+++ script/code.js
@@ -2,13 +2,13 @@
 // about section
 let about = document.querySelector('section#about .row:last-child');
 let aboutContents = [
-    "I am 18 years old.", "your mom"
+    "I am Ganiefah Majiet. I live in Parow, but I grew up in Bridgetown. I was born on the 18th of August, 2004. I speak both Afrikaans and English, but my home language is English. My highest level of education is matric, which I completed in 2022. Currently, I am enrolled in a full stack web development course at Life Choices Academy."
 ]
 
 //loop through the content 
-aboutContents.forEach( (content,i)=> {
+aboutContents.forEach( (content)=> {
     about.innerHTML += `
-      <p class='lead'>${i+1} ${content}</p>
+      <p class='lead'>${content}</p>
       `
 } ) //shortcut of creating a function
 
@@ -18,9 +18,14 @@ let education = [{
         id:1,
         year: 2022,
         description: 'Grade 12',
-        place: '',
-        type: 'Certificate',
-        certificate: ''
+        place: 'The Settlers High School',
+    },
+    {
+        id: 2,
+        year: 2023,
+        description: 'Currently busy studying a full stack web development course',
+        place: 'Life Choices',
+       
     },
 ]
 //
@@ -30,11 +35,92 @@ education.forEach((data)=> {
     <div class="card">
         <h4 class="display-4">${data.year}</h4>
         <div class="card-body">
-            <p class="text-white">
+            <p class="text-black">
                 ${data.description} @ <span>${data.place}</span> 
-                <a href="${data.certificate}" target="_blank">${data.type}</a>
             </p>
         </div>
     </div>
     `
 })
+ 
+//skills section
+//html
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 85) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
+//css
+var i = 0;
+function move1() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar1");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 90) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
+//bootstrap
+var i = 0;
+function move2() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 85) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
+// javascript 
+var i = 0;
+function move3() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar3");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 50) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
